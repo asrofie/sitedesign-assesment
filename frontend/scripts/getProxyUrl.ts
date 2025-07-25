0734cdc9091bcbc11a9d8f189b2390ab63f6a4a6
Muchammad Asrofie <asrofie@gmail.com>
2025-07-26 02:53:24 +0700

    fix missing API version in the proxy

--- frontend/scripts/getProxyUrl.ts
+++ frontend/scripts/getProxyUrl.ts
@@ -1,10 +1,10 @@
 export function getProxyUrl(env: 'development' | 'production') {
     const baseUrls = {
         development: {
-            API_BASE_URL: 'http://localhost:5000/api/**',
+            API_BASE_URL: 'http://localhost:5000/api/v1/**',
         },
         production: {
-            API_BASE_URL: 'http://localhost:5000/api/**',
+            API_BASE_URL: 'http://localhost:5000/api/v1/**',
         },
     };
 
